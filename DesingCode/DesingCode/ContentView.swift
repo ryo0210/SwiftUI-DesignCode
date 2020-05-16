07eb288c7fb771339f799d7ceca399729a3377e7
ryo0210 <yone0217@icloud.com>
2020-05-16 17:40:18 +0900

    I learned Animations and States.

--- DesingCode/DesingCode/ContentView.swift
+++ DesingCode/DesingCode/ContentView.swift
@@ -9,38 +9,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
+    
     var body: some View {
         ZStack {
             TitleView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
             
             BackCardView()
                 .background(Color("card4"))
                 .cornerRadius(20)
                 .shadow(radius: 10)
-                .offset(x: 0, y: -60)
+                .offset(x: 0, y: show ? -400 : -60)
                 .scaleEffect(0.9)
-                .rotationEffect(.degrees(10))
+                .rotationEffect(.degrees(show ? 0 : 10))
                 .rotation3DEffect(Angle(degrees: 10), axis: (x: 10.0, y: 0, z: 0))
                 .blendMode(.hardLight)
-            
-            Components and Visual Effects
+                //.animation(.spring(response: 0.5, dampingFraction: 0.8, blendDuration: 0))
+                //.animation(.easeInOut(duration: 0.5))
+                .animation(.easeOut(duration: 0.3))
             
             BackCardView()
                 .background(Color("card3"))
                 .cornerRadius(20)
                 .shadow(radius: 10)
-                .offset(x: 0, y: -30)
+                .offset(x: 0, y: show ? -200 : -30)
                 .scaleEffect(0.95)
-                .rotationEffect(.degrees(5))
+                .rotationEffect(.degrees(show ? 0 : 5))
                 .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
                 .blendMode(.hardLight)
+                //.animation(.spring(response: 0.5, dampingFraction: 0.8, blendDuration: 0))
+                //.animation(.easeInOut(duration: 0.5))
+                .animation(.easeOut(duration: 0.25))
             
             CardView()
                 //.blendMode(.hardLight)
+                .onTapGesture {
+                    self.show.toggle()
+            }
             
             ButtomCardView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20: 0)
+                .animation(.default)
         }
     }
 }
