dc145e011d06173273c98a678d1894317c27053b
ryo0210 <yone0217@icloud.com>
2020-05-17 17:23:06 +0900

    I learned Modal Presentation.

--- DesingCode/DesingCode/HomeView.swift
+++ DesingCode/DesingCode/HomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct HomeView: View {
     @Binding var showProfile: Bool
+    
     var body: some View {
         VStack {
             HStack {
@@ -27,7 +28,15 @@ struct HomeView: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 30) {
                     ForEach(sectionData) { item in
-                        SectionView(section: item)
+                        // GeometryReaderはビューの位置やサイズを検出するのに最適
+                        // スクロールするとカードのminX(左端)の値が変わる。
+                        // minXは急激に値が変化するので20で割って、変化量を抑えている。-30はパディング用
+                        GeometryReader { geometry in
+                            SectionView(section: item)
+                                .rotation3DEffect(Angle(degrees: Double(geometry.frame(in: .global).minX - 30) / -20
+                                ), axis: (x: 10.0, y: 10.0, z: 10.0))
+                        }
+                        .frame(width: 275, height: 275)
                     }
                 }
                 .padding(30)
@@ -86,6 +95,8 @@ struct Section: Identifiable {
 let sectionData = [
     Section(title: "Prototype", text: "18 section", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card2")), color: Color("card1")),
     Section(title: "Prototype", text: "18 section", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card4")), color: Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))),
+    Section(title: "Prototype", text: "18 section", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card4")), color: Color(#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1))),
+    Section(title: "Prototype", text: "18 section", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card4")), color: Color(#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1))),
     
 ]
 
