40b2ab2fb72045c24ce86f89abb6a0eb8302671d
ryo0210 <yone0217@icloud.com>
2020-05-17 22:49:25 +0900

    I learned Tab Bar and Test on Device.

---
+++ DesingCode/DesingCode/TabBar.swift
@@ -0,0 +1,30 @@
+//
+//  TabBar.swift
+//  DesingCode
+//
+//  Created by ryo on 2020/05/17.
+//  Copyright © 2020 ryo. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            Home().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("Home")
+            }
+            ContentView().tabItem {
+                Image(systemName: "rectangle.stack.fill")
+                Text("Certificates")
+            }
+        }
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar().previewDevice("iPhone Xs Max")
+    }
+}
