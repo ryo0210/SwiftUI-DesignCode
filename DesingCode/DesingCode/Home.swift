995cb254fcc86ed5b6d00173366ea034132e015c
ryo0210 <yone0217@icloud.com>
2020-05-17 15:56:23 +0900

    I learned Binding and Screen Size.

--- DesingCode/DesingCode/Home.swift
+++ DesingCode/DesingCode/Home.swift
@@ -16,26 +16,7 @@ struct Home: View {
         ZStack {
             Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
                 .edgesIgnoringSafeArea(.all)
-            VStack {
-                HStack {
-                    Text("Watching")
-                        .font(.system(size: 28, weight: .bold))
-                    
-                    Spacer()
-                    
-                    Button(action: { self.showProfile.toggle() }) {
-                        Image("kirby")
-                            .renderingMode(.original)
-                            .resizable()
-                            //.aspectRatio(contentMode: .fill)
-                            .frame(width: 36, height: 36)
-                            .clipShape(Circle())
-                    }
-                }
-                .padding(.horizontal)
-                .padding(.top, 30)
-                Spacer()
-            }
+            HomeView(showProfile: $showProfile)
                 //.padding(.top, 44)
                 .background(Color.white)
                 
@@ -46,26 +27,26 @@ struct Home: View {
                 .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
                 .scaleEffect(showProfile ? 0.9 : 1)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-            .edgesIgnoringSafeArea(.all)
+                .edgesIgnoringSafeArea(.all)
             
             MenuView()
                 .background(Color.black.opacity(0.001))
-                .offset(y: showProfile ? 0 : 1000)
+                .offset(y: showProfile ? 0 : screen.height)
                 .offset(y: viewState.height)
                 .animation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0))
                 .onTapGesture {
                     self.showProfile.toggle()
             }
-        .gesture(
-            DragGesture().onChanged { value in
-                self.viewState = value.translation
-            }
-            .onEnded { value in
-                if self.viewState.height > 100 {
-                    self.showProfile = false
+            .gesture(
+                DragGesture().onChanged { value in
+                    self.viewState = value.translation
                 }
-                self.viewState = .zero
-            }
+                .onEnded { value in
+                    if self.viewState.height > 100 {
+                        self.showProfile = false
+                    }
+                    self.viewState = .zero
+                }
             )
         }
     }
@@ -76,3 +57,20 @@ struct Home_Previews: PreviewProvider {
         Home()
     }
 }
+
+struct AvatarView: View {
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        Button(action: { self.showProfile.toggle() }) {
+            Image("kirby")
+                .renderingMode(.original)
+                .resizable()
+                //.aspectRatio(contentMode: .fill)
+                .frame(width: 36, height: 36)
+                .clipShape(Circle())
+        }
+    }
+}
+
+let screen = UIScreen.main.bounds
---
+++ DesingCode/DesingCode/HomeView.swift
@@ -0,0 +1,34 @@
+//
+//  HomeView.swift
+//  DesingCode
+//
+//  Created by ryo on 2020/05/17.
+//  Copyright © 2020 ryo. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var showProfile: Bool
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
+                
+                Spacer()
+                
+                AvatarView(showProfile: $showProfile)
+            }
+            .padding(.horizontal)
+            .padding(.top, 30)
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(showProfile: .constant(false))
+    }
+}
