0567541fdcf6fdad6f6113d1a0d040832204966e
ryo0210 <yone0217@icloud.com>
2020-05-18 00:36:32 +0900

    I learned Ring Component and Formul.

--- DesingCode/DesingCode/RingView.swift
+++ DesingCode/DesingCode/RingView.swift
@@ -9,25 +9,35 @@
 import SwiftUI
 
 struct RingView: View {
+    var color1 = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)
+    var color2 = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+    var circleWidth: CGFloat =  300
+    var circleHeight: CGFloat = 300
+    var percent: CGFloat = 80
+        
     var body: some View {
-        ZStack {
+        let multiplier = circleWidth / 44
+        let progress = 1 - (percent / 100)
+        
+        return ZStack {
             Circle()
-                .stroke(Color.black.opacity(0.1), style: StrokeStyle(lineWidth: 5))
-                .frame(width: 44, height: 44)
+                .stroke(Color.black.opacity(0.1), style: StrokeStyle(lineWidth: 5 * multiplier))
+                .frame(width: circleWidth, height: circleHeight)
             Circle()
-                .trim(from: 0.2, to: 1)
+                .trim(from: progress, to: 1)
                 .stroke(
-                    LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)), Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))]), startPoint: .topTrailing, endPoint: .bottomLeading),
-                    style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
+                    LinearGradient(gradient: Gradient(colors: [Color(color1), Color(color2)]), startPoint: .topTrailing, endPoint: .bottomLeading),
+                    style: StrokeStyle(lineWidth: 5 * multiplier, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
             )
             // 3時の方向からfrom: 0.2だけ欠けた状態で描画される。なので90度回転させることで6時から8時まで欠けている状態にする。
             //そして、degrees: 180で表裏をひっくり返し、円形のプログレスバーを描画している。
             .rotationEffect(Angle(degrees: 90))
             .rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y: 0, z: 0))
-            .frame(width: 44, height: 44)
-                .shadow(color: Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)).opacity(0.1), radius: 3, x: 0, y: 3)
+            .frame(width: circleWidth, height: circleHeight)
+                .shadow(color: Color(color2).opacity(0.1), radius: 3 * multiplier, x: 0, y: 3 * multiplier)
             
-            Text("80%")
+            Text("\(Int(percent))%")
+                .font(.system(size: 14 * multiplier))
                 .font(.subheadline)
                 .fontWeight(.bold)
         }
