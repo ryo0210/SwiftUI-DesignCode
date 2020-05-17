60d8f2511ea2498a858801c0a94c149635bfb6cf
ryo0210 <yone0217@icloud.com>
2020-05-17 18:27:52 +0900

    I learned Modal Presentation.

--- DesingCode/DesingCode/HomeView.swift
+++ DesingCode/DesingCode/HomeView.swift
@@ -10,16 +10,31 @@ import SwiftUI
 
 struct HomeView: View {
     @Binding var showProfile: Bool
+    @State var showUpdate = false
     
     var body: some View {
         VStack {
             HStack {
                 Text("Watching")
-                    .font(.system(size: 28, weight: .bold))
+                    .font(.system(size: 38, weight: .bold))
                 
                 Spacer()
                 
                 AvatarView(showProfile: $showProfile)
+                
+                Button(action: { self.showUpdate.toggle() }) {
+                Image(systemName: "bell")
+                    .renderingMode(.original)
+                    .font(.system(size: 20, weight: .medium))
+                    .frame(width: 36, height: 36)
+                    .background(Color.white)
+                    .clipShape(Circle())
+                    .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
+                }
+                .sheet(isPresented: $showUpdate) {
+                    ContentView()
+                }
             }
             .padding(.horizontal)
             .padding(.leading, 14)
