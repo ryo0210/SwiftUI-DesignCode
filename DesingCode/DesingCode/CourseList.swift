97178700b8d1d82b5d60cc88114fc234904136ff
ryo0210 <yone0217@icloud.com>
2020-05-19 01:03:09 +0900

    I learned Passing Index to Cards.

--- DesingCode/DesingCode/CourseList.swift
+++ DesingCode/DesingCode/CourseList.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct CourseList: View {
     @State var courses = courseData
     @State var active = false
+    @State var activeIndex = -1
     
     var body: some View {
         ZStack {
@@ -21,16 +22,25 @@ struct CourseList: View {
             ScrollView {
                 VStack(spacing: 30) {
                     Text("Courses")
-                    .font(.system(.largeTitle)).bold()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 30)
-                    .padding(.top, 30)
+                        .font(.system(.largeTitle)).bold()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                        .padding(.top, 30)
                         .blur(radius: active ? 20 : 0)
                     
                     ForEach(courses.indices, id: \.self) { index in
                         GeometryReader { geometry in
-                            CourseView(show: self.$courses[index].show, course: self.courses[index], active: self.$active)
-                                .offset(y: self.courses[index].show ? -geometry.frame(in: .global).minY : 0)
+                            CourseView(
+                                show: self.$courses[index].show,
+                                course: self.courses[index],
+                                active: self.$active,
+                                index: index,
+                                activeIndex: self.$activeIndex
+                            )
+                            .offset(y: self.courses[index].show ? -geometry.frame(in: .global).minY : 0)
+                            .opacity(self.activeIndex != index && self.active ? 0 : 1)
+                            .scaleEffect(self.activeIndex != index && self.active ? 0.5 : 1)
+                            .offset(x: self.activeIndex != index && self.active ? screen.width : 0)
                         }
                         .frame(height: 280)
                         .frame(maxWidth: self.courses[index].show ? .infinity : screen.width - 60)
@@ -56,6 +66,8 @@ struct CourseView: View {
     @Binding var show: Bool
     var course: Course
     @Binding var active : Bool
+    var index : Int
+    @Binding var activeIndex : Int
     
     var body: some View {
         ZStack(alignment: .top) {
@@ -120,6 +132,11 @@ struct CourseView: View {
             .onTapGesture {
                 self.show.toggle()
                 self.active.toggle()
+                if self.show {
+                   self.activeIndex = self.index
+                } else {
+                   self.activeIndex = -1
+                }
             }
         }
         .frame(height: show ? screen.height : 280)
