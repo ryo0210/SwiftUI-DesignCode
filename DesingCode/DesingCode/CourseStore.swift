836e1a4e6ea382591261ff478f930c9df4f72aa2
ryo0210 <yone0217@icloud.com>
2020-05-20 13:54:09 +0900

    I learned URL Images.

--- DesingCode/DesingCode/CourseStore.swift
+++ DesingCode/DesingCode/CourseStore.swift
@@ -31,14 +31,16 @@ class CourseStore: ObservableObject {
     @Published var courses: [Course] = courseData
     
     init() {
+        let colors = [#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1), #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1), #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)]
+        
         getArray(id: "course") { (items) in
             items.forEach { (item) in
                 self.courses.append(Course(
                     title: item.fields["title"] as! String,
                     subtitle: item.fields["subtitle"] as! String,
-                    image: #imageLiteral(resourceName: "Card4"),
+                    image: item.fields.linkedAsset(at: "image")?.url ?? URL(string: "")!,
                     logo: #imageLiteral(resourceName: "Logo1"),
-                    color: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),
+                    color: colors.randomElement()!,
                     show: false))
             }
         }
